58046a95d873ad6819f288661d69db3f4c099adf
snowy988 <natashj22@gmail.com>
2025-05-01 07:48:48 +0100

    Created creature.cs

---
+++ Creature.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+    public abstract class Creature
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+
+        public Creature(string name, int health)
+        {
+            Name = name;
+            Health = health;
+        }
+
+        public void TakeDamage(int amount)
+        {
+            Health -= amount;
+            if (Health < 0) Health = 0;
+        }
+
+        public abstract void Attack(Creature target);
+    }
+}
